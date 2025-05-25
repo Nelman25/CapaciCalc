06ac3e14df0a8c1e227d947a15044a8367e6d081
Jonel Villaver <jonelvillaver735@gmail.com>
2025-05-25 19:28:53 +0800

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        softBlue: {
+          100: "#F8FAFC",
+          200: "#D9EAFD",
+          300: "#BCCCDC",
+          400: "#9AA6B2",
+        },
+        skyBolt: "#0D99FF",
+      },
+    },
   },
   plugins: [],
 };
